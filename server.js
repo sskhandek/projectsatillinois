5a149cb6f3c61287f3b95e11334354101f8a9e17
Sujay Khandekar <sskhandek@gmail.com>
2015-10-06 02:02:33 -0500

    LOL

---
+++ server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by SujayKhandekar on 10/6/15.
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 3000);
